d1ca994aec85692231efc0f294e4b3a440a22a80
abraham-tl <abraham-tl>
2018-02-05 20:59:40 -0500

    calse 1
    se creo el player

---
+++ Bomberman/Assets/Scrip/Movimiento_player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento_player : MonoBehaviour {
+
+    public GameObject player_1;
+    public float speed;
+    public float speed_rotation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * speed_rotation;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+    }
+}
