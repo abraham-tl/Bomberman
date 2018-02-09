8787902c43e322e11b19dd04ead4fdfa268a15cc
abraham-tl <Abrahan Lopera>
2018-02-08 21:16:21 -0500

    detonacion
    Se inicia scrip detonacion

---
+++ Bomberman/Assets/Scrip/Denotacion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Denotacion : MonoBehaviour {
+
+    public  Rigidbody  dispositivo;
+	// Use this for initialization
+	void Start () {
+        dispositivo = GetComponent<Rigidbody>();
+	}
+	
+	
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "edificio" )
+        {
+            print("Hola");
+            dispositivo.velocity = Vector3.zero;
+        }
+    }
+}
