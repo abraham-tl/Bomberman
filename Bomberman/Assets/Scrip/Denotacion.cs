d52584fa72ba297e1d59a6e9e30b2e4dc4b8e43a
abraham-tl <abraham-tl>
2018-02-09 18:46:47 -0500

    tiempo detonador
    Se creo el tiempo del detonador

--- Bomberman/Assets/Scrip/Denotacion.cs
+++ Bomberman/Assets/Scrip/Denotacion.cs
@@ -5,19 +5,55 @@ using UnityEngine;
 public class Denotacion : MonoBehaviour {
 
     public  Rigidbody  dispositivo;
+    public bool instalacion;
+
+    
+    public Vector3 posicion;
+
+    bool detonador;
+    float tiempo; 
 	// Use this for initialization
 	void Start () {
         dispositivo = GetComponent<Rigidbody>();
+        instalacion = false;
+        detonador = false;
+        tiempo = 5;
 	}
-	
-	
+
+    private void Update()
+    {
+  
+        if (instalacion)
+        {
+            transform.position = posicion;
+            transform.rotation = Quaternion.identity;
+        }
+        if (detonador)
+        {
+            timer();
+            print(tiempo);
+        }
+        
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "edificio" )
         {
-            print("Hola");
-            dispositivo.velocity = Vector3.zero;
+            
+          dispositivo.velocity = Vector3.zero;
+            posicion = collision.contacts[0].point;
+            instalacion = true;
+            detonador = true;
+        }
+    }
+
+    private void timer()
+    {
+        tiempo = tiempo - Time.deltaTime;
+        if (tiempo <= 0)
+        {
+            detonador = false;
         }
     }
 }
---
+++ museum/Assets/MoverCamara.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverCamara : MonoBehaviour
+{
+    public float speed;
+
+	void Start () {
+		
+	}
+	
+	
+
+	void Update ()
+    {
+        if (Input.GetAxis("Mouse X") > 0) ;
+        transform.position = 
+	}
+}
