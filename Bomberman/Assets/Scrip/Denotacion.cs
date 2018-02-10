800a5445826346d4eb47b866f800a908c5114343
abraham-tl <abraham-tl>
2018-02-09 21:18:19 -0500

    otro
    otro

--- Bomberman/Assets/Scrip/Denotacion.cs
+++ Bomberman/Assets/Scrip/Denotacion.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Denotacion : MonoBehaviour {
 
+    public Explosion m_explocion;
+    public GameObject efecto;
     public  Rigidbody  dispositivo;
     public bool instalacion;
 
-    
+    public Text text_tiepo;
     public Vector3 posicion;
+
+    GameObject edificio_para_destruir;
 
     bool detonador;
     float tiempo; 
@@ -40,7 +45,7 @@ public class Denotacion : MonoBehaviour {
     {
         if (collision.gameObject.tag == "edificio" )
         {
-            
+            edificio_para_destruir = collision.gameObject;
           dispositivo.velocity = Vector3.zero;
             posicion = collision.contacts[0].point;
             instalacion = true;
@@ -51,9 +56,17 @@ public class Denotacion : MonoBehaviour {
     private void timer()
     {
         tiempo = tiempo - Time.deltaTime;
+
+        text_tiepo.text = ((int)tiempo).ToString();
         if (tiempo <= 0)
         {
             detonador = false;
+            GameObject explocion = Instantiate(efecto, posicion, Quaternion.identity);
+            m_explocion = explocion.GetComponent<Explosion>();
+            m_explocion.Desactivar_edificio(edificio_para_destruir);
+            Destroy(explocion, 1.5f);
+            Destroy(this.gameObject);
+
         }
     }
 }
---
+++ Bomberman/Assets/Scrip/Explosion.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+   public void Desactivar_edificio(GameObject edificio)
+    {
+        edificio.SetActive(false);
+    }
+}
